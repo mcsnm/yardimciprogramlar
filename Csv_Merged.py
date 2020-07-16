ea76b9fd96ae1c00a006e597350e90fba166fc3d
mcsnm <darkfuzuli@gmail.com>
2020-07-16 21:21:00 +0300

    Add files via upload

---
+++ Csv_Merged.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import glob
+import os
+
+
+#Bu program .csv uzantılı birden çok dosyayı birleştirmek için kullanılır
+
+
+
+os.getcwd()
+#Klasor uzantisini secin
+path = u"/Users/muratcansinim/Desktop/ASELSAN/_yapilan_calismalar/_Lidar_Verileri_200330/26022020_Lidar_Veriler"
+#liste = os.listdir(path)
+
+
+
+
+
+# Selecting csv files
+all_files = glob.glob(path + "/*.csv")
+
+# Reading and concating all Csv files
+all_csv = (pd.read_csv(f,encoding='UTF-16 LE') for f in all_files)
+df_merged   = pd.concat(all_csv, ignore_index=True)
+
+#Kayit adini belirleyin, bu dosyanin bulundugu klasorde olusacaktir.
+df_merged.to_csv( "merged.csv")
